6e425a8c4693d98b39cae3d1f76fc97e19483508
Hogue <ajhogue6@gmail.com>
2015-07-13 14:58:34 -0400

    adds geolocation on map load

--- app/controllers/mapController.js
+++ app/controllers/mapController.js
@@ -7,32 +7,59 @@ var MapController = function($scope, $log, $timeout, uiGmapGoogleMapApi) {
       areaLng      = -100.2471641,
       areaZoom     = 3;
 
-  $scope.count = 0;
+  $scope.count = 1;
+
+
 
   var events = {
     places_changed: function (searchBox) {
       var query = searchBox.getPlaces()[0];
       var bathroom = {};
-      bathroom.latitude = query.geometry.location.k;
       bathroom.longitude = query.geometry.location.D;
+      bathroom.latitude = query.geometry.location.k;
       $scope.count++;
       bathroom.id = $scope.count;
       bathroom.title = location.formatted_address;
       $scope.bathrooms.push(bathroom);
-      console.log(query);
+      // console.log(query);
     }
   }
 
   $scope.searchbox = { template:'searchbox.tpl.html', events:events};
   $scope.bathrooms = [];
+  $scope.currentLocation = [];
 
-  uiGmapGoogleMapApi.then(function(maps) {
-    $scope.map     = { center: { latitude: areaLat, longitude: areaLng }, zoom: areaZoom};
-    $scope.options = { scrollwheel: false };
 
-  });
+  if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(function(position) {
+        var pos = {
+          longitude: position.coords.longitude,
+          latitude: position.coords.latitude,
+          id: 0,
+          title: "current location"
+        };
+        console.log(pos);
+        $scope.currentLocation.push(pos);
 
- };
+      uiGmapGoogleMapApi.then(function(maps) {
+        $scope.map     = {
+          center: {
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude
+          },
+          zoom: areaZoom
+        };
+        $scope.options = { scrollwheel: false };
+      });
+    }, function(error){
+      console.error(error);
+    }, {
+      maximumAge: 0
+    });
+
+    }
+
+  };
 
  // MapController.$inject = ['mapFactory', 'appSettings'];
 
--- app/services/mapFactory.js
+++ app/services/mapFactory.js
@@ -1,18 +1,18 @@
-(function mapFactoryIIFE(){
+// (function mapFactoryIIFE(){
 
-  app.controller("mapController", function( uiGmapGoogleMapApi) {
+//   app.controller("mapController", function( uiGmapGoogleMapApi) {
 
-  // Define variables for our Map object
-  var areaLat      = 44.2126995,
-      areaLng      = -100.2471641,
-      areaZoom     = 3;
+//   // Define variables for our Map object
+//   var areaLat      = 44.2126995,
+//       areaLng      = -100.2471641,
+//       areaZoom     = 3;
 
-  uiGmapGoogleMapApi.then(function(maps) {
-    this.map     = { center: { latitude: areaLat, longitude: areaLng }, zoom: areaZoom };
-    this.options = { scrollwheel: false };
-  });
+//   uiGmapGoogleMapApi.then(function(maps) {
+//     this.map     = { center: { latitude: areaLat, longitude: areaLng }, zoom: areaZoom };
+//     this.options = { scrollwheel: false };
+//   });
 
-});
+// });
 
-  angular.module('mapApp').factory('mapFactory', mapFactory);
-})();
+//   angular.module('mapApp').factory('mapFactory', mapFactory);
+// })();
